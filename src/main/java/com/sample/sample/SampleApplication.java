11f2734806d2d328ae48fa38eb87b54e142924e7
YourAKShaw <hitme@yourakshaw.com>
2024-08-29 20:31:21 +0530

    feat(server): :sparkles: add GET / route for Hello World!

--- src/main/java/com/sample/sample/SampleApplication.java
+++ src/main/java/com/sample/sample/SampleApplication.java
@@ -2,12 +2,22 @@ package com.sample.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 @SpringBootApplication
 public class SampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
 
+    @Bean
+    public RouterFunction<ServerResponse> helloWorldRoute() {
+        return route(GET("/"), request -> ServerResponse.ok().bodyValue("Hello World!"));
+    }
 }
