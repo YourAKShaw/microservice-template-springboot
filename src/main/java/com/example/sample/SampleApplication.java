1137113f85594822c697f464df58dca4075db3e1
YourAKShaw <hitme@yourakshaw.com>
2024-08-30 17:18:42 +0530

    refactor(repo): :recycle: rename the company from sample to example

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -1,4 +1,4 @@
-package com.sample.sample;
+package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/sample/SampleApplicationTests.java
+++ src/test/java/com/example/sample/SampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sample.sample;
+package com.example.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
